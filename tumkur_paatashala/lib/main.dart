89ef9a95445118cb7805542d680dcc11dd54b8f8
Akshathg4268 <74520341+Akshathg4268@users.noreply.github.com>
2023-05-05 08:28:33 +0530

    testing auth

--- tumkur_paatashala/lib/main.dart
+++ tumkur_paatashala/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tumkur_paatashala/screens/auth_testing.dart';
 
 import 'firebase_options.dart';
 
@@ -16,10 +17,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("Hello World"),
+          child: AuthTesting(),
         ),
       ),
     );
